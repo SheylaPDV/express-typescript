e6bd7893ad41bef6ada5bd40afbb9ac53c0a7d4a
Sheyla <sheylapdv@gmail.com>
2023-06-30 14:10:11 +0200

    add types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+// Aqui creamos los tipos
+export type Weather = "sunny" | "rainy" | "cloudy " | "windy" | "stormy";
+export type Visibility = "great" | "good" | "ok" | "poor";
+
+// Aqui se define el "contrato"
+export interface DiaryEntry {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment: string;
+}
+
+// Esto lo que hace es heredar todos los tipos de DiaryEntry + el nuevo tipo creado. se puede hacer con type pero la sintaxis es menos legible
+// interface SpecialDiaryEntry extends DiaryEntry {
+//     flightNumber: number
+// }
