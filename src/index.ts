b83b5ba4fb7c067eb2fda4c66d6e0c29c54940cd
Sheyla <sheylapdv@gmail.com>
2023-06-30 13:09:10 +0200

    init express

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express"; // ESModules, cuando se compila, usa common Js
+// const express = require('express') -> commonjs
+import diaryRouter from "./routes/diaries";
+const app = express();
+
+app.use(express.json()); // middleware que transforma la req.body a un json
+
+const PORT = 3000;
+app.get("/ping", (_, res) => {
+  console.log("someone pinged here!!" + new Date().toLocaleDateString());
+  res.send("pong");
+});
+
+// Las rutas se crean aqui
+app.use("/api/diaries", diaryRouter);
+
+app.listen(PORT, () => {
+  console.log("Server running on port ${PORT}");
+});
