83d31d7a607cf0bac084dd63af1410ca1032707f
Sheyla <sheylapdv@gmail.com>
2023-07-01 19:10:00 +0200

    add type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,15 +1,20 @@
 // Aqui creamos los tipos
-export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "stormy";
-export type Visibility = "great" | "good" | "ok" | "poor";
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
+export type Visibility = 'great' | 'good' | 'ok' | 'poor'
 
 // Aqui se define el "contrato"
 export interface DiaryEntry {
-  id: number;
-  date: string;
-  weather: Weather;
-  visibility: Visibility;
-  comment: string;
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
 }
+
+// export type NonSensitiveInfoDiaryEntry = pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+
+// De esta forma, se queda con todos menos con comentario.
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
 
 // Esto lo que hace es heredar todos los tipos de DiaryEntry + el nuevo tipo creado. se puede hacer con type pero la sintaxis es menos legible
 // interface SpecialDiaryEntry extends DiaryEntry {
