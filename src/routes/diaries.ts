b4033c086f182c446cc5505de8204e8cd2b56310
Sheyla <sheylapdv@gmail.com>
2023-07-01 19:09:45 +0200

    add routes

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,13 +1,29 @@
-import express from "express";
+import express from 'express'
+import * as diaryServices from '../services/diaryServices'
+const router = express.Router()
 
-const router = express.Router();
+// Ruta para traer datos
+router.get('/', (_req, res) => {
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
+})
 
-router.get("/", (_req, res) => {
-  res.send("Fetching all entry diaries");
-});
+// Ruta para traer datos por id
+router.get('/:id', (req, res) => {
+  // Los params siempre vienen como string, si necesitas number hay que transofrmarlo(con operador unario + o con Number)
+  const diary = diaryServices.findById(+req.params.id)
 
-router.post("/", (_req, res) => {
-  res.send("");
-});
+  return (diary != null) ? res.send(diary) : res.sendStatus(404)
+  // res.send(diary?.weather)
+})
 
-export default router;
+// Ruta para crear datos
+router.post('/', (req, res) => {
+  // Introduce los datos en el body
+  const { date, weather, visibility, comment } = req.body
+  // Se llama al metodo addEntry para crear nuevos datos
+  const newDiaryEntry = diaryServices.addDiary(date, weather, visibility, comment)
+  // Se transforma a json
+  res.json(newDiaryEntry)
+})
+
+export default router
